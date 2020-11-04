c6ff8bebd80d94b0a6a76ae538cfd87b14ba5651
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2020-11-03 22:56:52 -0800

    Red_Queen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React,{useRef,useEffect} from 'react';
 import './App.css';
+import {Red_Queen} from './Red_Queen';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() {  
+  return(
+    <div>
+      <Red_Queen />
+      </div>
+  )
+  
 }
 
 export default App;
---
+++ src/Red_Queen.js
@@ -0,0 +1,128 @@
+import React, { useEffect, useRef } from "react";
+import "./App.css";
+
+
+export const Red_Queen = () => {
+  let alice_Sprite = useRef(null);
+  let foreground = useRef(null);
+  let background = useRef(null);
+
+  useEffect(() => {
+    let spriteFrames = [
+      { transform: "translateY(0)" },
+      { transform: "translateY(-100%)" },
+    ];
+
+    let alice = alice_Sprite.current.animate(spriteFrames, {
+      easing: "steps(7, end)",
+      direction: "reverse",
+      duration: 1200,
+      playbackRate: 1,
+      iterations: Infinity,
+    });
+
+    setInterval(function () {
+      if (alice.playbackRate > 0.4) {
+        alice.playbackRate -= 0.1;
+        adjustSceneryPlayback();
+      }
+    }, 3000);
+
+    let sceneryFrames = [
+      { transform: "translateX(100%)" },
+      { transform: "translateX(-100%)" },
+    ];
+
+    let sceneryTimingBackground = {
+      duration: 6000,
+      iterations: Infinity,
+    };
+
+    let sceneryTimingForeground = {
+      duration: 2000,
+      iterations: Infinity,
+    };
+
+    let foregroundMovement = foreground.current.animate(
+      sceneryFrames,
+      sceneryTimingForeground
+    );
+    let backgroundMovement = background.current.animate(
+      sceneryFrames,
+      sceneryTimingBackground
+    );
+
+    let sceneries = [foregroundMovement, backgroundMovement];
+
+    let adjustSceneryPlayback = () => {
+
+
+
+      if (alice.playbackRate < 0.8) {
+        sceneries.forEach(function (anim) {
+          anim.playbackRate = (alice.playbackRate / 2) * -1;
+        });
+      } else if (alice.playbackRate > 1.2) {
+        sceneries.forEach(function (anim) {
+          anim.playbackRate = alice.playbackRate / 2;
+        });
+      } else {
+        sceneries.forEach(function (anim) {
+          anim.playbackRate = 0;
+        });
+      }
+    };
+    adjustSceneryPlayback();
+
+    const goFaster = () => {
+      alice.playbackRate += 0.2;
+      adjustSceneryPlayback();
+    };
+
+    window.addEventListener("click", goFaster);
+  });
+
+  return (
+    <div className="main_div">
+      <div className="sky"></div>
+      <div className="earth">
+        <div className="alice">
+          <img
+            className="alice_Sprite"
+            ref={alice_Sprite}
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png"
+            alt=" "
+          />
+        </div>
+      </div>
+
+      <div className="scenery" id="foreground" ref={foreground}>
+        <img
+          id="treefore"
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png"
+          alt=" "
+        />
+      </div>
+
+      <div className="scenery background1" ref={background}>
+        <img
+          className="pawn"
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png"
+          alt=" "
+        />
+        <img
+          className="pawn2"
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png"
+          alt=" "
+        />
+        <img
+          className="treeback"
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png"
+          alt=" "
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Red_Queen;
